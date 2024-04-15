79a776a46e5e5c40d5042def637f7b3ccf35fd7b
Jules418 <julio34310@gmail.com>
2024-04-15 14:59:26 +0200

    work in progress

--- src/env.c
+++ src/env.c
@@ -1,47 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   structures.h                                       :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbanacze <jbanacze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/01 18:40:51 by jules             #+#    #+#             */
-/*   Updated: 2024/04/15 13:26:02 by jbanacze         ###   ########.fr       */
+/*   Created: 2024/04/15 14:36:28 by jbanacze          #+#    #+#             */
+/*   Updated: 2024/04/15 14:55:50 by jbanacze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCTURES_H
-# define STRUCTURES_H
+#include "minishell.h"
 
-# define CMD 1
-# define ARG 2
-# define INFILE 3
-# define INFILE_HEREDOC 4
-# define 
-# define 
-# define 
-
-typedef struct s_token	*t_token;
-typedef struct s_env	*t_env;
-
-struct s_token
+size_t	env_len(t_env env)
 {
-	char	*str;
-	int		type;
-	t_token	prev;
-	t_token	next;
-};
+	if (!env)
+		return (0);
+	return (1 + env_len->next);
+}
 
-struct s_env
+char	**tenv_to_arr(t_env	env)
 {
-	char	*value;
-	t_env	next;
-};
+	char	**envp;
+	size_t	i;
+	size_t	len;
 
-typedef struct s_minishell
-{
-	t_token	cmd_line;
-	t_env	env;
-}	*t_minishell;
-
-#endif
+	len = env_len(env) + 1;
+	envp = malloc(sizeof(char *) * len);
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		envp[i] = env->value;
+		env = env->next;
+		i++;
+	}
+	envp[i] = NULL;
+	return (envp);
+}
--- src/parsing/process_input.c
+++ src/parsing/process_input.c
@@ -3,11 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   process_input.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jbanacze <jbanacze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:38:53 by jules             #+#    #+#             */
-/*   Updated: 2024/04/10 05:13:52 by jules            ###   ########.fr       */
+/*   Updated: 2024/04/15 12:35:03 by jbanacze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
+#include "minishell.h"
+
+t_token	new_token(char *str)
+{
+	t_token	tok;
+
+	if (!str)
+		return (NULL);
+	tok = malloc(sizeof(struct s_token));
+	if (!tok)
+		return (NULL);
+	tok->next = NULL;
+	tok->prev = NULL;
+	tok->str = str;
+	return (tok);
+}
+
+void free_token(t_token tok)
+{
+	if (!tok)
+		return ;
+	free(tok->str);
+	free(tok);
+}
+
+t_token	token_from_arr(char **arr, t_token prev)
+{
+	t_token	tok;
+
+	if (!arr || !(arr[0]))
+		return (NULL);
+	tok = malloc(sizeof(struct s_token));
+	if (!tok)
+		return (NULL);
+	tok->prev = prev;
+	tok->str = arr[0];
+	tok->next = token_from_arr(arr + 1, tok);
+	if (!(tok->next) && arr[1])
+	{
+		free_token(tok);
+		return (NULL);
+	}
+	return (tok);
+}
+
+t_token	tokenize_input(char *line)
+{
+	char	**splitted_input;
+	t_token	toknized_line;
+
+	splitted_input = ft_split(line, ' ');
+	if (!splitted_input)
+		return (NULL);
+	toknized_line = token_from_arr(splitted_input);
+}
